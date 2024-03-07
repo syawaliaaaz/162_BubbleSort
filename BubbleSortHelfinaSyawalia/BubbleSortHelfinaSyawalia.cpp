700272546e4841dd601b8c7f0f5c5443a746cca1
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-03-07 13:32:43 +0700

    Prosedur Mengurutkan Array dengan Metode Bubble Sort

--- BubbleSortHelfinaSyawalia/BubbleSortHelfinaSyawalia.cpp
+++ BubbleSortHelfinaSyawalia/BubbleSortHelfinaSyawalia.cpp
@@ -38,6 +38,18 @@ void display() {                                          //Prosedur untuk menam
     cout << endl;                                         //Output baris kosong
 }
 
+void bubbleSortArray() {                  //Prosedur untuk mengurutkan array dengan metode bubble sort
+    for (int i = 1; i < n; i++) {         //Looping dengan i dimulai dari 1 hingga n-1
+        for (int j = 0; j < n - i; j++) { //Looping dengan j dimulai dari 0 hingga n-1
+            if (a[j] > a[j + 1]) {        //Jika nilai pada a[j] lebih besar dari a[j+1]
+                int temp = a[j];          //Simpan nilai a[j] ke variabel sementara temp
+                a[j] = a[j + 1];          //Assign nilai a[j+1] ke a[j]
+                a[j + 1] = temp;          //Assign nilai temp ke a[j+1]
+            }
+        }
+    }
+}
+
 
 
 
