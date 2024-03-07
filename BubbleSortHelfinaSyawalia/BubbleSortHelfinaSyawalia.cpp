f7bd31ed35168af522670723c2ebee4ee30df231
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-03-07 13:07:38 +0700

    Add project files.

---
+++ BubbleSortHelfinaSyawalia/BubbleSortHelfinaSyawalia.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+using namespace std;
+
+
+int a[20]; //Deklarasi array a dengan ukuran 20
+int n;     //Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
+
+void input() {                                                        //Prosedur untuk input
+    while (true) {                                                    //Looping
+        cout << "Masukan banyaknya elemen pada array : ";             //Output ke layar
+        cin >> n;                                                     //Input dari pengguna
+        if (n <= 20)                                                  //Jika n kurang dari atau sama dengan 20
+            break;                                                    //Keluar dari loop
+        else {                                                        //Jika n lebih dari 20
+            cout << "\nArray dapat mempunyai maksimal 20 elemen. \n"; //Output ke layar
+        }
+    }
+    cout << endl;                           //Output baris kosong
+    cout << "====================" << endl; //Output ke layar
+    cout << "Masukan Elemen Array" << endl; //Output ke layar
+    cout << "====================" << endl; //Output ke layar
+     
+    for (int i = 0; i < n; i++) {              //Looping dengan i dimulai dari 0 hingga n-1
+        cout << "Data Ke-" << (i + 1) << ": "; //Output ke layar
+        cin >> a[i];                           //Input dari pengguna
+    }
+}
+
+
+
+    
